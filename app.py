8bfc8c4e419f9ec5b5cff3a0c3ec6df290153a21
tesvn7 <tesvn7@gmail.com>
2024-04-17 16:28:54 +0530

    1.1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import pandas as pd
 
 # Team array
 teams = ['Sunrisers Hyderabad',
@@ -21,12 +22,19 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Sharjah', 'Mohali', 'Bengaluru']
 
 # Loading the model via pickle file
-pipe = pickle.load(open('pipe.pkl','rb'))
+try:
+    pipe = pickle.load(open('pipe.pkl','rb'))
+except FileNotFoundError:
+    st.error('Model file "pipe.pkl" not found.')
+except Exception as e:
+    st.error( e)
+    raise
+
 
 # Heading Title
 st.title('IPL Win Predictor')
 
-col1, col2 = st.beta_columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
     batting_team = st.selectbox('Select the batting team',sorted(teams))
@@ -37,7 +45,7 @@ selected_city = st.selectbox('Select host city',sorted(cities))
 
 target = st.number_input('Target')
 
-col3,col4,col5 = st.beta_columns(3)
+col3,col4,col5 = st.columns(3)
 
 with col3:
     score = st.number_input('Score')
@@ -53,10 +61,26 @@ if st.button('Predict Probability'):
     crr = score/overs
     rrr = (runs_left*6)/balls_left
 
-    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+    input_df = pd.DataFrame({
+        'batting_team': [batting_team],
+    'bowling_team': [bowling_team],
+    'city': [selected_city],
+    'runs_left': [runs_left],
+    'balls_left': [balls_left],
+    'wickets': [wickets],
+    'total_runs_x': [target],
+    'crr': [crr],
+    'rrr': [rrr]
+})
 
-    result = pipe.predict_proba(input_df)
-    loss = result[0][0]
-    win = result[0][1]
-    st.header(batting_team + "- " + str(round(win*100)) + "%")
-    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+    try:
+        result = pipe.predict_proba(input_df)
+        loss = result[0][0]
+        win = result[0][1]
+        st.header(batting_team + "- " + str(round(win*100)) + "%")
+        st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+
+    except Exception as e:
+        st.error( e)
+        raise
+    
